b8b2aff13ee4d3c26ddbd1e76f2d083f97757268
tdeason416 <tdeason416@gmail.com>
2017-10-12 17:37:00 -0700

    initiated the git repo

---
+++ histogram.r
@@ -0,0 +1,19 @@
+#======================================================
+# Complete the following 2 TODOs
+# MSDS6306: Lab8, Jongwook Woo
+#======================================================
+
+
+# reads the list of words from words.txt y
+words <- readLines("words.txt")
+
+# TODO: Define length of words using nchar(wrods): words_length <-
+words_length <- 
+
+# TODO: define hist_dat using table(words_length): hist_dat <-
+hist_dat <- 
+
+# writes the table of word length frequency
+write.table(hist_dat, "histogram.tsv",
+            sep = "\t", row.names = FALSE, quote = FALSE)
+  
