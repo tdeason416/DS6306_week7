d2fe1fc6059196a1f98ba178b55578d81aaa17ef
tdeason416 <tdeason416@gmail.com>
2017-10-12 18:02:50 -0700

    completed make exersize

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
 all: words.txt
 
 clean:
-	rm -f words.txt
-
+	rm -f words.txt histogram.txt
+							
 words.txt:
 	Rscript -e 'download.file("https://svnweb.freebsd.org/base/head/share/dict/web2?view=co", destfile = "words.txt", method="curl", quiet = TRUE)'
+
+histogram.tsv: histogram.r words.txt
+
+
 
--- histogram.r
+++ histogram.r
@@ -8,12 +8,11 @@
 words <- readLines("words.txt")
 
 # TODO: Define length of words using nchar(wrods): words_length <-
-words_length <- 
+words_length <- sapply(words, nchar)
 
 # TODO: define hist_dat using table(words_length): hist_dat <-
-hist_dat <- 
+hist_dat <- table(words_length)
 
 # writes the table of word length frequency
 write.table(hist_dat, "histogram.tsv",
             sep = "\t", row.names = FALSE, quote = FALSE)
-  
